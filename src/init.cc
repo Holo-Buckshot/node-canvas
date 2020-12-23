2740c63c9cd65c91d8d7e69aac6ea61c8b0f89f9
Holo-Buckshot <6374088+Holo-Buckshot@users.noreply.github.com>
2020-12-23 14:59:16 -0500

    Make Context Aware

--- src/init.cc
+++ src/init.cc
@@ -89,4 +89,6 @@ NAN_MODULE_INIT(init) {
   Nan::Set(target, Nan::New<String>("freetypeVersion").ToLocalChecked(), Nan::New<String>(freetype_version).ToLocalChecked()).Check();
 }
 
-NODE_MODULE(canvas, init);
+NODE_MODULE_INIT() {
+ Init(exports);   
+}
